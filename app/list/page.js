d39651ecc7e3d1c2e5a51ac91e5cb69f64ed68c3
Jihoon <jangna96@gmail.com>
2023-05-02 18:08:44 +0900

    feat: List페이지 상품리스트 렌더링
    - product 배열에 있는 데이터 렌더링
    - 이미지 및 상품명 추가
    - map 메소드 활용

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,45 @@
+
 export default function List() {
+  let products = [
+    {
+      id: 1,
+      name: "Tomatoes",
+      price: 40,
+      src: "food0.png",
+      alt: "토마토",
+    },
+    {
+      id: 2,
+      name: "Pasta",
+      price: 30,
+      src: "food1.png",
+      alt: "파스타",
+    },
+    {
+      id: 3,
+      name: "Coconut",
+      price: 20,
+      src: "food2.png",
+      alt: "코코넛",
+    },
+  ];
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {products.map((item) => {
+        return (
+          <div className="food" key={item.id}>
+            <img
+              className="food-img"
+              src={`/food/${item.src}`}
+              alt={item.alt}
+            />
+            <h4>
+              {item.name} ${item.price}
+            </h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
